18781e127d6e4f58db5929619a0bf08ebf669e19
RodPSouza <rod.p.souza@hotmail.com>
2019-10-20 18:25:11 -0300

    Curso Angular - Balta.IO Aula de 0 a 16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
+import { FormBuilder} from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todov02';
+  public todos: Todo[] = [];
+  public title: String = 'Minhas tarefas';
+  public form: FormGroup;
+  
+  constructor(private fb: FormBuilder){
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
+      ])]
+    });
+    
+    this.todos.push(new Todo(1,'Passear com o cachorro',false));
+    this.todos.push(new Todo(2,'Ir ao supermercado',false));
+    this.todos.push(new Todo(3,'Cortar o cabelo',true));
+  }
+
+  add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.clear();
+  }  
+
+  clear(){
+
+  }
+
+  remove(todo: Todo){
+    const  index = this.todos.indexOf(todo);
+    if(index !== -1){
+      this.todos.splice(index,1);
+    }
+  }
+
+  markAsDone(todo: Todo){
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo){
+    todo.done = false;
+  }
+
 }
